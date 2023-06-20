80035c0f228da2c97f41f34788ebaed2970f49cd
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-20 14:07:34 +0600

    Проверено дз от 30.05
    Комментарии к коду

--- HW 5/Task 2(HW 5).py
+++ HW 5/Task 2(HW 5).py
@@ -8,10 +8,12 @@ else:
     # number_1 = user_input / 100
     # number_2 = (user_input / 10) % 10
     # number_3 = (user_input % 10)
-
-    number_1 = user_input / 100
-    number_2 = (user_input / 1000) % 10
-    number_3 = (user_input / 100) % 10
+                                         # VN: допустим, user_input = 456
+    number_1 = user_input / 100          # здесь 4.56
+    number_2 = (user_input / 1000) % 10  # здесь 0.456
+    number_3 = (user_input / 100) % 10   # здесь 4.56
+                                         # и программа думает, что 1я цифра == 3я цифра
+                                         # хотя это не так
 
     if(number_1 == number_2 or number_2 == number_3 or number_1 == number_3):
         print("Цифры совпали")
