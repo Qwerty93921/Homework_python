c7897799974de2189dd5f413e4036f5e0043aa77
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-27 16:10:02 +0600

    Проверено дз от 02.06
    Комментарии к коду

--- HW 6(Functions)/Task 2(HW 6).py
+++ HW 6(Functions)/Task 2(HW 6).py
@@ -1,6 +1,6 @@
 def get_sum(num):
-    if num == 1:
-        return 1
+    if num == 1:  # VN: для надёжности лучше использовать <=
+        return 1  # И возвращать не 1, а num. Вдруг вам дробное число передадут в функцию
     return num + get_sum(num-1)
 
 num = int(input("Введите целое число: "))
--- HW 6(Functions)/Task 8(HW 6).py
+++ HW 6(Functions)/Task 8(HW 6).py
@@ -3,6 +3,7 @@ def total_time(seconds):
     minutes_from_seconds = (seconds % 3600) // 60
     seconds_from_seconds = (seconds % 3600) % 60
     time = "{:02d}:{:02d}:{:02d}".format(hours_from_seconds, minutes_from_seconds, seconds_from_seconds)
+    # VN: этот способ сильно лучше, чем в задаче 6
     return time
 
 user_input_1 = int(input("Введите секунды: "))
