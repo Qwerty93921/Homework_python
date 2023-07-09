e9204f7ca5ba942eef324ca62fb2db71d89e3641
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-07-09 18:24:04 +0600

    new methods
    new methods

---
+++ HW 14(ФВП Методы строк)/input change.py
@@ -0,0 +1,7 @@
+e1 = input("Введите что-либо:").upper()
+print(e1)
+
+e2 = input().lower().replace("e", "w").upper()  # Можно ВИДОИЗМЕНЯТЬ ВВОД ПОЛЬЗОВАТЕЛЯ
+print(e2) # privet надо ввести
+
+# PRIVWT
