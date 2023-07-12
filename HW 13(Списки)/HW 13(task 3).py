6bf1128b809e4953444285709f49643c3a113ed3
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-07-13 04:47:39 +0600

    asd
    asd

--- HW 13(Списки)/HW 13(task 3).py
+++ HW 13(Списки)/HW 13(task 3).py
@@ -3,8 +3,11 @@ my_list = [1,2,3,4,5,6,7,8,9,10]
 min_number = min(my_list)
 max_number = max(my_list)
 
+min_index = my_list.index(min_number)
+max_index = my_list.index(max_number)
+
 changed_list = my_list.copy()
-changed_list[0] = max_number
-changed_list[9] = min_number
+changed_list[min_index] = max_number
+changed_list[max_index] = min_number
 
 print(changed_list)
