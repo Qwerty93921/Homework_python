d2fbec16950d7a87799ded3e6c65e5daa71fa929
Qwerty93921 <134071623+Qwerty93921@users.noreply.github.com>
2023-08-28 16:55:40 +0600

    HW 20(Calculator)
    HW 20(Calculator)

--- HW 20(VENV)/HW 20(VENV).py
+++ HW 20(VENV)/HW 20(VENV).py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import *
 
-# Set-ExecutionPolicy -Scope CurrentUser RemoteSigned - чтобы заработало
+# Set-ExecutionPolicy -Scope CurrentUser RemoteSigned - чтобы заработало виртуальное окружение
 # Set-ExecutionPolicy RemoteSigned -Scope Process - (первый вариант - не точно)
 # https://ru.stackoverflow.com/questions/935212/powershell-%D0%B2%D1%8B%D0%BF%D0%BE%D0%BB%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D1%81%D1%86%D0%B5%D0%BD%D0%B0%D1%80%D0%B8%D0%B5%D0%B2-%D0%BE%D1%82%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%BE-%D0%B2-%D1%8D%D1%82%D0%BE%D0%B9-%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B5
 
@@ -38,10 +38,10 @@ class Calculator(QWidget):
         layout.addLayout(grid_of_buttons)
         self.setLayout(layout)
 
-def main_calculator():
+def calculator_activate():
     app = QApplication([])
     calculator = Calculator()
     calculator.show()
     app.exec_()
 
-main_calculator()
+calculator_activate()
